5b8402f0a3b96209931b39ef99aaae346ba3c41f
vandinhvanessa <56143482+vandinhvanessa@users.noreply.github.com>
2019-10-07 11:09:30 -0700

    Add files via upload

---
+++ comet-blaster/Vanessa's Game Shooter/GameScene.swift
@@ -0,0 +1,58 @@
+//
+//  GameScene.swift
+//  Vanessa's Game Shooter
+//
+//  Created by Vanessa Van Dinh on 3/12/19.
+//  Copyright © 2019 Vanessa's Apps. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+     let player = SKSpriteNode(imageNamed:"playerShip")
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+        
+       
+        player.setScale(1)
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height/5)
+        player.zPosition = 2
+        self.addChild(player)
+    }
+    
+    func fireBullet(){
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([moveBullet, deleteBullet])
+        bullet.run(bulletSequence)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireBullet()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches{
+            let pointOfTouch = touch.location(in:self)
+            let previousPointOfTouch = touch.previousLocation(in:self)
+            let amountDragged = pointOfTouch.x - previousPointOfTouch.x
+            
+            player.position.x += amountDragged
+        }
+    }
+    
+}
---
+++ comet-blaster/Vanessa's Game ShooterTests/Vanessa_s_Game_ShooterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Vanessa_s_Game_ShooterTests.swift
+//  Vanessa's Game ShooterTests
+//
+//  Created by Vanessa Van Dinh on 3/12/19.
+//  Copyright © 2019 Vanessa's Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Vanessa_s_Game_Shooter
+
+class Vanessa_s_Game_ShooterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
